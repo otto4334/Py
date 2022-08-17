5544438c8a95b3d5ddd980ed176c55a69ec8a5ee
otto4334 <47845578+otto4334@users.noreply.github.com>
2022-08-17 07:54:02 -0500

    Add files via upload

---
+++ mpg calc.py
@@ -0,0 +1,7 @@
+#Calculate mpg
+miles_driven = input("Enter miles driven:")
+miles_driven=float(miles_driven)
+gallons_used=input("Enter gallons used:")
+gallons_used=float(gallons_used)
+mpg=miles_driven/gallons_used
+print("Miles per gallon:", mpg)
